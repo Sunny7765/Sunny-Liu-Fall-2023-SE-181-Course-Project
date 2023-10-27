4843af97aec4415c6e140c15e469c3e626cc91aa
Sunny <sunnyliu776>
2023-10-27 14:02:41 -0400

    Assignment 4: Moved all similar tests to AccountTest for Assignment 4.

--- src/test/java/AccountTest.java
+++ src/test/java/AccountTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AccountTest {
 
-    Checking checking;
+    Account checking;
 
     @BeforeEach
     public void setUp() {
@@ -13,13 +13,51 @@ public class AccountTest {
     }
 
     @Test
-    public void deposit_to_account() {
-        double initialBalance = checking.getBalance();
-        double depositAmount = 420.00;
-
-        checking.depositBalance(depositAmount);
-
-        assertEquals(depositAmount, 420.00);
+    public void bank_account_created_with_apr() {
+        assertEquals(0.6, checking.getApr());
     }
 
+    @Test
+    public void deposit_to_checking_account() {
+        double initialBalance = checking.getBalance();
+        double depositAmount = 420.0;
+        double newBalance = checking.depositBalance(depositAmount);
+
+        assertEquals(initialBalance + depositAmount, newBalance, 0.001);
+    }
+
+    @Test
+    public void withdraw_from_checking_account() {
+        checking.depositBalance(420.00);
+        double initialBalance = checking.getBalance();
+        double withdrawAmount = 100.0;
+        checking.withdrawBalance(withdrawAmount);
+        double newBalance = checking.getBalance();
+
+        assertEquals(initialBalance - withdrawAmount, newBalance, 0.001);
+    }
+
+    @Test
+    public void withdraws_can_not_go_below_0() {
+        checking.depositBalance(50);
+        checking.withdrawBalance(100);
+        double newBalance = checking.getBalance();
+
+        assertEquals(0.0, newBalance, 0.001);
+    }
+
+    @Test
+    public void deposit_twice_into_the_same_account() {
+        checking.depositBalance(100.0);
+        checking.depositBalance(150.0);
+        assertEquals(250.0, checking.getBalance(), 0.001);
+    }
+
+    @Test
+    public void withdraw_twice_from_the_same_account() {
+        checking.depositBalance(300.0);
+        checking.withdrawBalance(50.0);
+        checking.withdrawBalance(75.0);
+        assertEquals(175.0, checking.getBalance(), 0.001);
+    }
 }
--- src/test/java/CDTest.java
+++ src/test/java/CDTest.java
@@ -20,24 +20,4 @@ public class CDTest {
 
         assertEquals(initialBalance + depositAmount, newBalance, 0.001);
     }
-
-    @Test
-    public void withdraw_from_CD_account() {
-        cd.depositBalance(420.00);
-        double initialBalance = cd.getBalance();
-        double withdrawAmount = 100.0;
-        cd.withdrawBalance(withdrawAmount);
-        double newBalance = cd.getBalance();
-
-        assertEquals(initialBalance - withdrawAmount, newBalance, 0.001);
-    }
-
-    @Test
-    public void withdraws_can_not_go_below_0() {
-        cd.depositBalance(50);
-        cd.withdrawBalance(100);
-        double newBalance = cd.getBalance();
-
-        assertEquals(950.0, newBalance, 0.001);
-    }
 }
